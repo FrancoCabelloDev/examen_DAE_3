d47e9de14148ddcf822c96f4ea841243d3ea5798
FrancoCabelloDev <franco_alex_07@hotmail.com>
2025-06-04 11:44:15 -0500

    se termino airbnb

---
+++ airbnb-spa/src/data/popularDestinations.js
@@ -0,0 +1,25 @@
+// src/data/popularDestinations.js
+
+export const popularDestinations = [
+  {
+    id: 'cusco',
+    name: 'Cusco',
+    imageUrl: '/images/cusco.jpg',        // ← RUTA relativa a "public/"
+  },
+  {
+    id: 'arequipa',
+    name: 'Arequipa',
+    imageUrl: '/images/arequipa.jpg',
+  },
+  {
+    id: 'mancora',
+    name: 'Máncora',
+    imageUrl: '/images/mancora.jpg',
+  },
+  {
+    id: 'ayacucho',
+    name: 'Ayacucho',
+    imageUrl: '/images/ayacucho.jpg',
+  },
+  // Agrega más destinos si quieres...
+];
